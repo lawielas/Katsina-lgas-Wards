4109988774e21cafc7732d7e0279901e6ddb2541
Lawal Aminu Saleh <lawie.aminu@yahoo.com>
2024-09-30 08:08:50 +0100

    wards updated

---
+++ Katsina.ts
@@ -0,0 +1,569 @@
+export const katsina =  [
+        {
+          "id": 1,
+          "name": "Bakori",
+          "wards": [
+            {"id": 1, "name": "Jargaba"},
+            {"id": 2, "name": "Kakumi"},
+            {"id": 3, "name": "Kandarawa"},
+            {"id": 4, "name": "Kurami/Yankwani"},
+            {"id": 5, "name": "Tsiga"},
+            {"id": 6, "name": "Kabomo"},
+            {"id": 7, "name": "Guga"},
+            {"id": 8, "name": "Dawan Musa"},
+            {"id": 9, "name": "Bakori 'A'"},
+            {"id": 10, "name": "Bakori 'B'"},
+            {"id": 224, "name": "Bbarde-kwantakwaran"}
+          ]
+        },
+        {
+          "id": 2,
+          "name": "Batagarawa",
+          "wards": [
+            {"id": 11, "name": "Ajiwa"},
+            {"id": 12, "name": "Barawa"},
+            {"id": 13, "name": "Batagarawa"},
+            {"id": 14, "name": "Dandagoro"},
+            {"id": 15, "name": "Jino"},
+            {"id": 16, "name": "Kayauki"},
+            {"id": 17, "name": "Kware"},
+            {"id": 18, "name": "Tsanni"},
+            {"id": 19, "name": "Yargamji"},
+            {"id": 225, "name": "Bakiyawa"}
+          ]
+        },
+        {
+          "id": 3,
+          "name": "Batsari",
+          "wards": [
+            {"id": 20, "name": "Abadau-kagara"},
+            {"id": 21, "name": "Batsari"},
+            {"id": 23, "name": "Dandume"},
+            {"id": 24, "name": "Madogara"},
+            {"id": 25, "name": "Manawa"},
+            {"id": 26, "name": "Ruma"},
+            {"id": 27, "name": "Wagini"},
+            {"id": 28, "name": "Yauyau Mallamawa"},
+            {"id": 226, "name": "karare"},
+            {"id": 227, "name": "kandawa"},
+            {"id": 228, "name": "darini-magaji-abu"},
+            {"id": 229, "name": "dan-alh-yangaiya"},
+          ]
+        },
+        {
+          "id": 4,
+          "name": "Baure",
+          "wards": [
+            {"id": 1, "name": "Agala"},
+            {"id": 2, "name": "Baure"},
+            {"id": 3, "name": "Mai-Bara"},
+            {"id": 4, "name": "Taramnawa"},
+            {"id": 5, "name": "Yanduna"},
+            {"id": 6, "name": "Yanmaulu"},
+            {"id": 7, "name": "babban-mutum"},
+            {"id": 8, "name": "garki"},
+            {"id": 9, "name": "hui"},
+            {"id": 10, "name": "kagara-faski"},
+            {"id": 11, "name": "muduri"},
+            {"id": 12, "name": "unguwar-rai"},
+          ]
+        },
+        {
+          "id": 5,
+          "name": "Bindawa",
+          "wards": [
+            {"id": 1, "name": "Baure"},
+            {"id": 2, "name": "Bindawa"},
+            {"id": 3, "name": "faru-dallaji"},
+            {"id": 4, "name": "Doro"},
+            {"id": 5, "name": "faru-dallaji"},
+            {"id": 6, "name": "Gaiwa"},
+            {"id": 7, "name": "Jibawa"},
+            {"id": 8, "name": "Kamri"},
+            {"id": 9, "name": "Shibdawa"},
+            {"id": 10, "name": "Tama"},
+            {"id": 11, "name": "yangora"}
+          ]
+        },
+        {
+          "id": 6,
+          "name": "Charanchi",
+          "wards": [
+            {"id": 1, "name": "Banye"},
+            {"id": 2, "name": "Charanchi"},
+            {"id": 3, "name": "Doka"},
+            {"id": 4, "name": "Ganuwa"},
+            {"id": 5, "name": "Koda"},
+            {"id": 6, "name": "Kuraye"},
+            {"id": 7, "name": "Majen Wayya"},
+            {"id": 8, "name": "Radda"},
+            {"id": 9, "name": "Safana"},
+            {"id": 10, "name": "Tsakatsa"}
+          ]
+        },
+        {
+          "id": 7,
+          "name": "Dandume",
+          "wards": [
+            {"id": 1, "name": "Dandume A"},
+            {"id": 2, "name": "Dandume B"},
+            {"id": 3, "name": "Dantankari"},
+            {"id": 4, "name": "Magaji-Wando-A"},
+            {"id": 5, "name": "Magaji-Wando-B"},
+            {"id": 6, "name": "Mahuta A"},
+            {"id": 7, "name": "Mahuta B"},
+            {"id": 8, "name": "Mahuta C"},
+            {"id": 9, "name": "Nasarawa"},
+            {"id": 10, "name": "Tumburkai A"},
+            {"id": 11, "name": "Tumburkai B"},
+          ]
+        },
+        {
+          "id": 8,
+          "name": "Danja",
+          "wards": [
+            {"id": 1, "name": "Dabai"},
+            {"id": 2, "name": "Danja A"},
+            {"id": 3, "name": "Danja B"},
+            {"id": 4, "name": "Jiba"},
+            {"id": 5, "name": "Kahutu A"},
+            {"id": 6, "name": "Kahutu B"},
+            {"id": 7, "name": "Tandama"},
+            {"id": 8, "name": "Tsangamawa"},
+            {"id": 9, "name": "Yakaji A"},
+            {"id": 10, "name": "Yakaji B"}
+          ]
+        },
+        {
+          "id": 9,
+          "name": "Dan Musa",
+          "wards": [
+            {"id": 1, "name": "Dan Ali"},
+            {"id": 2, "name": "Dan Alkima"},
+            {"id": 3, "name": "Dan Musa A"},
+            {"id": 4, "name": "Dan Musa B"},
+            {"id": 5, "name": "Dandire A"},
+            {"id": 6, "name": "Dandire B"},
+            {"id": 7, "name": "Mai dabino A"},
+            {"id": 8, "name": "Mai dabino B"},
+            {"id": 9, "name": "mara"},
+            {"id": 10, "name": "Yan Tumaki A"},
+            {"id": 11, "name": "Yan Tumaki B"}
+          ]
+        },
+        {
+          "id": 10,
+          "name": "Daura",
+          "wards": [
+            {"id": 1, "name": "Kusugu"},
+            {"id": 2, "name": "Madobi A"},
+            {"id": 3, "name": "Madobi B"},
+            {"id": 4, "name": "Mazoji A"},
+            {"id": 5, "name": "Mazoji B"},
+            {"id": 6, "name": "Sabon Gari"},
+            {"id": 7, "name": "Sarkin Yara A"},
+            {"id": 8, "name": "Sarkin Yara B"},
+            {"id": 9, "name": "Tudun Wada"},
+            {"id": 10, "name": "Ubandawaki A"},
+            {"id": 11, "name": "Ubandawaki B"}
+          ]
+        },
+        {
+          "id": 11,
+          "name": "Dutsi",
+          "wards": [
+            {"id": 1, "name": "Dan Aunai"},
+            {"id": 2, "name": "Dutsi A"},
+            {"id": 3, "name": "Dutsi B"},
+            {"id": 4, "name": "Kayawa"},
+            {"id": 5, "name": "Ruwankaya A"},
+            {"id": 6, "name": "Ruwankaya B"},
+            {"id": 7, "name": "Sirika A"},
+            {"id": 8, "name": "Sirika B"},
+            {"id": 9, "name": "Yamel A"},
+            {"id": 10, "name": "Yamel B"}
+          ]
+        },
+        {
+          "id": 12,
+          "name": "Dutsin Ma",
+          "wards": [
+            {"id": 1, "name": "Bagagadi"},
+            {"id": 2, "name": "Dabawa"},
+            {"id": 3, "name": "Dutsin Ma A"},
+            {"id": 4, "name": "Dutsin Ma B"},
+            {"id": 5, "name": "Karofi A"},
+            {"id": 6, "name": "Karofi B"},
+            {"id": 7, "name": "Kuki A"},
+            {"id": 8, "name": "Kuka B"},
+            {"id": 9, "name": "Kutawa"},
+            {"id": 10, "name": "Makera"},
+            {"id": 11, "name": "Shema"}
+          ]
+        },
+        {
+          "id": 13,
+          "name": "Faskari",
+          "wards": [
+            {"id": 1, "name": "Daudawa"},
+            {"id": 2, "name": "Faskari"},
+            {"id": 3, "name": "Maigora"},
+            {"id": 4, "name": "Mairuwa"},
+            {"id": 5, "name": "Ruwan-Godiya"},
+            {"id": 6, "name": "Sabon Layin Galadima"},
+            {"id": 7, "name": "Sheme"},
+            {"id": 8, "name": "Tafoki"},
+            {"id": 9, "name": "Yankara"},
+            {"id": 10, "name": "Yarmalamai"}
+          ]
+        },
+        {
+          "id": 14,
+          "name": "Funtua",
+          "wards": [
+            {"id": 1, "name": "Dandutse"},
+            {"id": 2, "name": "Dukke"},
+            {"id": 3, "name": "Goya"},
+            {"id": 4, "name": "Mai Gamji"},
+            {"id": 5, "name": "Makera"},
+            {"id": 6, "name": "Maska"},
+            {"id": 7, "name": "Sabon Gari"},
+            {"id": 8, "name": "Tudun Iya"},
+            {"id": 9, "name": "Ung Ibrahim"},
+            {"id": 10, "name": "Unguwar Musa"},
+            {"id": 11, "name": "Unguwar Rabiu"}
+          ]
+        },
+        {
+          "id": 15,
+          "name": "Ingawa",
+          "wards": [
+            {"id": 1, "name": "Agayawa"},
+            {"id": 2, "name": "Bareruwa Ruruma"},
+            {"id": 3, "name": "Bidore Yaya"},
+            {"id": 4, "name": "Dara"},
+            {"id": 5, "name": "Daunaka B Kwari"},
+            {"id": 6, "name": "Dugul"},
+            {"id": 7, "name": "Ingawa"},
+            {"id": 8, "name": "Jobe Kandawa"},
+            {"id": 9, "name": "Kurfeji Yankaura"},
+            {"id": 10, "name": "Manomawa Kafi"},
+            {"id": 11, "name": "Yandoma"}
+          ]
+        },
+        {
+          "id": 16,
+          "name": "Jibia",
+          "wards": [
+            {"id": 1, "name": "Bugaje"},
+            {"id": 2, "name": "Farfaru"},
+            {"id": 3, "name": "Faru"},
+            {"id": 4, "name": "G Baure Mallamawa"},
+            {"id": 5, "name": "Gangara"},
+            {"id": 6, "name": "Jibia A"},
+            {"id": 7, "name": "Jibia B"},
+            {"id": 8, "name": "Kusa"},
+            {"id": 9, "name": "Mazanya Magama"},
+            {"id": 10, "name": "Riko"},
+            {"id": 11, "name": "Yangaiya"}
+          ]
+        },
+        {
+          "id": 17,
+          "name": "Kafur",
+          "wards": [
+            {"id": 1, "name": "Dantutture"},
+            {"id": 2, "name": "Dutsin Kura Kanya"},
+            {"id": 3, "name": "Gamzago"},
+            {"id": 4, "name": "Gozaki"},
+            {"id": 5, "name": "Kafur"},
+            {"id": 6, "name": "Mahuta"},
+            {"id": 7, "name": "Masari"},
+            {"id": 8, "name": "Sabuwar Kasa"},
+            {"id": 9, "name": "Yari Bori"},
+            {"id": 10, "name": "Yartalata Rigoji"}
+          ]
+        },
+        {
+          "id": 18,
+          "name": "Kaita",
+          "wards": [
+            {"id": 1, "name": "Abdallawa"},
+            {"id": 2, "name": "Baawa"},
+            {"id": 3, "name": "Dankaba"},
+            {"id": 4, "name": "Dankama"},
+            {"id": 5, "name": "Gafiya"},
+            {"id": 6, "name": "Girka"},
+            {"id": 7, "name": "Kaita"},
+            {"id": 8, "name": "Matsai"},
+            {"id": 9, "name": "Yandaki"},
+            {"id": 10, "name": "Yanhoho"}
+          ]
+        },
+        {
+          "id": 19,
+          "name": "Kankara",
+          "wards": [
+            {"id": 1, "name": "Burdugau"},
+            {"id": 2, "name": "Dan Murabu"},
+            {"id": 3, "name": "Dan Maidaki"},
+            {"id": 4, "name": "Garagi"},
+            {"id": 5, "name": "Gatakawa"},
+            {"id": 6, "name": "Hurya"},
+            {"id": 7, "name": "Kankara"},
+            {"id": 8, "name": "Kukasheka"},
+            {"id": 9, "name": "Pauwa"},
+            {"id": 10, "name": "Wawar Kaza"},
+            {"id": 11, "name": "Zango Zabaro"}
+          ]
+        },
+        {
+          "id": 20,
+          "name": "Kankia",
+          "wards": [
+            {"id": 1, "name": "Gachi"},
+            {"id": 2, "name": "Galadima A"},
+            {"id": 3, "name": "Galadima B"},
+            {"id": 4, "name": "Kafin Dangi Fakuwa"},
+            {"id": 5, "name": "Kafinsoli"},
+            {"id": 6, "name": "Kunduru Gyaza"},
+            {"id": 7, "name": "Magam Tsa"},
+            {"id": 8, "name": "Rimaye"},
+            {"id": 9, "name": "Sukuntuni"},
+            {"id": 10, "name": "Tafashiya Nasarawa"}
+          ]
+        },
+        {
+          "id": 21,
+          "name": "Katsina",
+          "wards": [
+            {"id": 1, "name": "Kangiwa"},
+            {"id": 2, "name": "Shinkafi A"},
+            {"id": 3, "name": "Shinkafi B"},
+            {"id": 4, "name": "Wakili Kudu iii"},
+            {"id": 5, "name": "Wakilin Arewa A"},
+            {"id": 6, "name": "Wakilin Arewa B"},
+            {"id": 7, "name": "Wakilin Gabas i"},
+            {"id": 8, "name": "Wakilin Gabas ii"},
+            {"id": 9, "name": "Wakilin Kudu i"},
+            {"id": 10, "name": "Wakilin Kudu ii"},
+            {"id": 11, "name": "Wakilin Yamma i"},
+            {"id": 12, "name": "Wakilin Yamma ii"}
+          ]
+        },
+        {
+          "id": 22,
+          "name": "Kurfi",
+          "wards": [
+            {"id": 1, "name": "Barkiyya"},
+            {"id": 2, "name": "birchi"},
+            {"id": 3, "name": "Kurfi A"},
+            {"id": 4, "name": "Kurfi B"},
+            {"id": 5, "name": "Rawayau A"},
+            {"id": 6, "name": "Rawayau B"},
+            {"id": 7, "name": "Tsauri A"},
+            {"id": 8, "name": "Tsauri B"},
+            {"id": 9, "name": "Wurma A"},
+            {"id": 10, "name": "Wurma B"}
+          ]
+        },
+        {
+          "id": 23,
+          "name": "Kusada",
+          "wards": [
+            {"id": 1, "name": "Bauranya A"},
+            {"id": 2, "name": "Bauranya B"},
+            {"id": 3, "name": "Boko"},
+            {"id": 4, "name": "Dudunni"},
+            {"id": 5, "name": "Kaikai"},
+            {"id": 6, "name": "Kofa"},
+            {"id": 7, "name": "Kusada"},
+            {"id": 8, "name": "Mawashi"},
+            {"id": 9, "name": "Yashe A"},
+            {"id": 10, "name": "Yashe B"}
+          ]
+        },
+        {
+          "id": 24,
+          "name": "Mai'Adua",
+          "wards": [
+            {"id": 1, "name": "Bumbum A"},
+            {"id": 2, "name": "Bumbum B"},
+            {"id": 3, "name": "Danyashe"},
+            {"id": 4, "name": "Koza"},
+            {"id": 5, "name": "Mai Koni A"},
+            {"id": 6, "name": "Mai Koni B"},
+            {"id": 7, "name": "Mai'Adua A"},
+            {"id": 8, "name": "Mai'Adua B"},
+            {"id": 9, "name": "Mai'Adua C"},
+            {"id": 10, "name": "Natsalle"}
+          ]
+        },
+        {
+          "id": 25,
+          "name": "Malumfashi",
+          "wards": [
+            {"id": 1, "name": "Borin Dawa"},
+            {"id": 2, "name": "Dansarai"},
+            {"id": 3, "name": "Dayi"},
+            {"id": 4, "name": "Gorar Dansaka"},
+            {"id": 5, "name": "Karfi"},
+            {"id": 6, "name": "Makaurachi"},
+            {"id": 7, "name": "Malumfashi A"},
+            {"id": 8, "name": "Malumfashi B"},
+            {"id": 9, "name": "Na Alma"},
+            {"id": 10, "name": "Rawan Sanyi"},
+            {"id": 11, "name": "Yaba"},
+            {"id": 12, "name": "Yarmama"}
+          ]
+        },
+        {
+          "id": 26,
+          "name": "Mani",
+          "wards": [
+            {"id": 1, "name": "Bagiwa"},
+            {"id": 2, "name": "Bujawa Gewayau"},
+            {"id": 3, "name": "Duwan Makau"},
+            {"id": 4, "name": "Hamcheta"},
+            {"id": 5, "name": "Jani"},
+            {"id": 6, "name": "Kwatta"},
+            {"id": 7, "name": "Machika"},
+            {"id": 8, "name": "Magami"},
+            {"id": 9, "name": "mani"},
+            {"id": 10, "name": "Muduru"},
+            {"id": 11, "name": "Tsagem Takusheyi"}
+          ]
+        },
+        {
+          "id": 27,
+          "name": "Mashi",
+          "wards": [
+            {"id": 1, "name": "Bamble"},
+            {"id": 2, "name": "Doguru A"},
+            {"id": 3, "name": "Doguru B"},
+            {"id": 4, "name": "Gallu"},
+            {"id": 5, "name": "Jigawa"},
+            {"id": 6, "name": "Karau"},
+            {"id": 7, "name": "Mashi"},
+            {"id": 8, "name": "S Rijiya"},
+            {"id": 9, "name": "Sonkaya"},
+            {"id": 10, "name": "Tamilo A"},
+            {"id": 11, "name": "Tamilo B"}
+          ]
+        },
+        {
+          "id": 28,
+          "name": "Matazu",
+          "wards": [
+            {"id": 1, "name": "Dissi"},
+            {"id": 2, "name": "Gwarjo"},
+            {"id": 3, "name": "Karaduwa"},
+            {"id": 4, "name": "Kogari"},
+            {"id": 5, "name": "Matazu A"},
+            {"id": 6, "name": "Matazu B"},
+            {"id": 7, "name": "Mazoji A"},
+            {"id": 8, "name": "Mazoji B"},
+            {"id": 9, "name": "Rinjin Idi"},
+            {"id": 10, "name": "Sayaya"}
+          ]
+        },
+        {
+          "id": 29,
+          "name": "Musawa",
+          "wards": [
+            {"id": 1, "name": "Dangani"},
+            {"id": 2, "name": "Danjanku Karachi"},
+            {"id": 3, "name": "Garu"},
+            {"id": 4, "name": "Gingin"},
+            {"id": 5, "name": "Jikamshi"},
+            {"id": 6, "name": "Kira"},
+            {"id": 7, "name": "Kurkujan A"},
+            {"id": 8, "name": "Kurkujan B"},
+            {"id": 9, "name": "Musawa"},
+            {"id": 10, "name": "Tuge"},
+            {"id": 10, "name": "Yaradau Tabanni"}
+          ]
+        },
+        {
+          "id": 30,
+          "name": "Rimi",
+          "wards": [
+            {"id": 1, "name": "Abukur"},
+            {"id": 2, "name": "Fardami"},
+            {"id": 3, "name": "Kadandani"},
+            {"id": 4, "name": "Majengobir"},
+            {"id": 5, "name": "Makurda"},
+            {"id": 6, "name": "Masabo Kurabau"},
+            {"id": 7, "name": "Remawa Iyatawa"},
+            {"id": 8, "name": "Rimi"},
+            {"id": 9, "name": "sabon Garin Alarain"},
+            {"id": 10, "name": "Tsagero"}
+          ]
+        },
+        {
+          "id": 31,
+          "name": "Sabuwa",
+          "wards": [
+            {"id": 1, "name": "Damari"},
+            {"id": 2, "name": "Dugun Mu'azu"},
+            {"id": 3, "name": "Gamji"},
+            {"id": 4, "name": "Gazari"},
+            {"id": 5, "name": "Machika"},
+            {"id": 6, "name": "Maibakko"},
+            {"id": 7, "name": "Rafin Iwa"},
+            {"id": 8, "name": "Sabuwa A"},
+            {"id": 9, "name": "Sabuwa B"},
+            {"id": 10, "name": "Sayau"}
+          ]
+        },
+        {
+          "id": 32,
+          "name": "Safana",
+          "wards": [
+            {"id": 216, "name": "Babban Duhu A"},
+            {"id": 216, "name": "Babban Duhu B"},
+            {"id": 216, "name": "Baure A"},
+            {"id": 216, "name": "Baure B"},
+            {"id": 216, "name": "Runka A"},
+            {"id": 216, "name": "Runka B"},
+            {"id": 216, "name": "Safana"},
+            {"id": 216, "name": "Tsaskiya"},
+            {"id": 217, "name": "Zakka A"},
+            {"id": 217, "name": "Zakka B"}
+          ]
+        },
+        {
+          "id": 33,
+          "name": "Sandamu",
+          "wards": [
+            {"id": 219, "name": "Daneji A"},
+            {"id": 219, "name": "Daneji B"},
+            {"id": 219, "name": "Fago A"},
+            {"id": 219, "name": "Fago B"},
+            {"id": 219, "name": "Kagare"},
+            {"id": 219, "name": "Karkarku"},
+            {"id": 219, "name": "Katsayal"},
+            {"id": 219, "name": "Kwasarawa"},
+            {"id": 219, "name": "Rade A"},
+            {"id": 219, "name": "Rade B"},
+            {"id": 219, "name": "Sandamu"}
+          ]
+        },
+        {
+          "id": 34,
+          "name": "Zango",
+          "wards": [
+            {"id": 222, "name": "Dargage"},
+            {"id": 222, "name": "Garni"},
+            {"id": 222, "name": "Gwamba"},
+            {"id": 222, "name": "Kanda"},
+            {"id": 222, "name": "Kawarin Kudi"},
+            {"id": 222, "name": "Kawarin Malawamai"},
+            {"id": 222, "name": "Rogogo Cidari"},
+            {"id": 222, "name": "Sara"},
+            {"id": 222, "name": "Yardaje"},
+            {"id": 222, "name": "Zango"}
+          ]
+        }
+      ]
+  
